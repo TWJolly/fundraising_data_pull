a8b31788193467970c12a5d5d8ad5fcb3b599728
TWJolly <tobiasjolly@gmail.com>
2018-04-07 19:04:59 +0100

    moved r scripts into specific folders moved effective cahrites table to data folder and stopped ignoring full data folder, just ignoring big data sets

--- main.R
+++ main.R
@@ -4,8 +4,8 @@ library(jsonlite)
 library(rlist)
 library(XML)
 
-charities_csv <- 'effective_charities.csv' #replace with your list of preferred charities (this script currently only uses charity name and JustGiving ID)
 data_folder <- 'data'
+charities_csv <- file.path(data_folder, 'effective_charities.csv') #replace with your list of preferred charities (this script currently only uses charity name and JustGiving ID)
 snapshots_folder <- file.path(data_folder, 'just_giving_data_snapshots') #folder where the data ends up
 
 #these file paths are defined here used to save the data at the end of this script
@@ -21,6 +21,6 @@ all_experimental_pages <- file.path(data_folder, 'experimental_pages.csv')
 table_of_data_pulls <- file.path(data_folder, 'data_pulls.csv')
 
 source("my_app_id.R") #sources the file you just created with your app id on JustGiving
-source("functions.R")
-source("just_giving_data_pull.R")
-source("get_current_state_and_randomise.R")
+source("R/functions.R")
+source("R/just_giving_data_pull.R")
+source("R/get_current_state_and_randomise.R")
